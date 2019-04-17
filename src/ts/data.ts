88a7dea85d6a7ac370ab6b19938065ad19d5025a
Aliaksandr Pletsiazhou <alexandr.pletezhov@gmail.com>
2019-04-18 01:08:57 +0300

    changed path to images

--- src/ts/data.ts
+++ src/ts/data.ts
@@ -1,83 +1,83 @@
 export default {
     symbols: [
         {
-            filename: "./src/assets/img/symbols/01.png",
+            filename: "assets/img/symbols/01.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/02.png",
+            filename: "assets/img/symbols/02.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/03.png",
+            filename: "assets/img/symbols/03.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/04.png",
+            filename: "assets/img/symbols/04.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/05.png",
+            filename: "assets/img/symbols/05.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/06.png",
+            filename: "assets/img/symbols/06.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/07.png",
+            filename: "assets/img/symbols/07.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/08.png",
+            filename: "assets/img/symbols/08.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/09.png",
+            filename: "assets/img/symbols/09.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/10.png",
+            filename: "assets/img/symbols/10.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/11.png",
+            filename: "assets/img/symbols/11.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/12.png",
+            filename: "assets/img/symbols/12.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/symbols/13.png",
+            filename: "assets/img/symbols/13.png",
             texture: null,
         },
     ],
     buttonState: [
         {
-            filename: "./src/assets/img/btn_spin_disable.png",
+            filename: "assets/img/btn_spin_disable.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/btn_spin_hover.png",
+            filename: "assets/img/btn_spin_hover.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/btn_spin_normal.png",
+            filename: "assets/img/btn_spin_normal.png",
             texture: null,
         },
         {
-            filename: "./src/assets/img/btn_spin_pressed.png",
+            filename: "assets/img/btn_spin_pressed.png",
             texture: null,
         },
 
     ],
     border: {
-        filename: "./src/assets/img/slotOverlay.png",
+        filename: "assets/img/slotOverlay.png",
         texture: null,
     },
     frameBackground: {
-        filename: "./src/assets/img/winningFrameBackground.jpg",
+        filename: "assets/img/winningFrameBackground.jpg",
         texture: null,
     }
 }
